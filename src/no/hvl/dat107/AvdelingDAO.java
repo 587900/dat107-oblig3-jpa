5efd2852e3022e010af991feedc910a35e7713df
LEB <LEB@localhost>
2021-04-15 20:19:17 +0200

    Samme som sist

--- src/no/hvl/dat107/AvdelingDAO.java
+++ src/no/hvl/dat107/AvdelingDAO.java
@@ -3,9 +3,7 @@ package no.hvl.dat107;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 
 public class AvdelingDAO {
 	
